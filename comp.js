5e7eabda45a9e80256afe6e5cb938b2cd20f5f47
VKSawna <varindersawna01@gmail.com>
2025-06-17 15:49:09 +0530

    comparision

---
+++ comp.js
@@ -0,0 +1,11 @@
+let num1 = 10
+let num2 = "10"
+str1 = "Hello"
+str2 =""
+let a;
+console.log(num1 > num2)
+console.log(str1 == "Hello")
+console.log(str2 == null)
+console.log(undefined == null)
+console.log(a == null)
+console.log(num1 === num2)
